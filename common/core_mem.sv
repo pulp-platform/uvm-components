4d598a69e20a7dfbd8f9f46f62dcba2bd3e88f16
Florian Zaruba <fl.zaruba@be4web.net>
2017-07-25 13:00:15 +0200

    Fix missing comma

--- common/core_mem.sv
+++ common/core_mem.sv
@@ -88,7 +88,7 @@ module core_mem (
         .clk_i     ( clk_i           ),
         .rst_ni    ( rst_ni          ),
         .address_i ( instr_address_q ),
-        .data_o    ( fetch_data_rom  )
+        .data_o    ( fetch_data_rom  ),
         .data_q_o  (                 ),
         .req_i     (                 ),
         .grant_o   (                 ),
@@ -99,7 +99,7 @@ module core_mem (
         .clk_i     ( clk_i                      ),
         .rst_ni    ( rst_ni                     ),
         .address_i ( {5'b0, data_address, 3'b0} ),
-        .data_o    ( data_rom                   )
+        .data_o    ( data_rom                   ),
         .data_q_o  (                            ),
         .req_i     (                            ),
         .grant_o   (                            ),
